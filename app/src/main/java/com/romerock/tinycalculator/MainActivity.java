663fa8be4fd034be702749268d4272f212165937
ebricko <efabian@tecif.com>
2016-11-18 16:13:05 -0600

    First Commit

---
+++ app/src/main/java/com/romerock/tinycalculator/MainActivity.java
@@ -0,0 +1,108 @@
+package com.romerock.tinycalculator;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.crashlytics.android.Crashlytics;
+import com.romerock.calculator.Calculator;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import io.fabric.sdk.android.Fabric;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.calcEfect1)
+    Button calcEfect1;
+    @BindView(R.id.calcEfect2)
+    Button calcEfect2;
+    @BindView(R.id.calcEfect3)
+    Button calcEfect3;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                String shareBody = getString(R.string.share_text_msn);
+                sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, getString(R.string.romerock));
+                sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                startActivity(Intent.createChooser(sharingIntent, "Share via"));
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_visitanos) {
+            Uri uri = Uri.parse(getString(R.string.romerock_site)); // missing 'http://' will cause crashed
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick({R.id.calcEfect1, R.id.calcEfect2, R.id.calcEfect3})
+    public void onClick(View view) {
+        Calculator calc=new Calculator(this, 0);
+        View v = calc;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(v)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                    }
+                });
+        int optionEfect=0;
+        switch (view.getId()) {
+            case R.id.calcEfect1:
+                optionEfect=0;
+                break;
+            case R.id.calcEfect2:
+                optionEfect=1;
+                break;
+            case R.id.calcEfect3:
+                optionEfect=2;
+                break;
+        }
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().getAttributes().windowAnimations =calc.Animation(optionEfect);
+        dialog.show();
+    }
+}
---
+++ calculator/src/main/java/com/romerock/calculator/Calculator.java
@@ -0,0 +1,231 @@
+package com.romerock.calculator;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Ebricko on 10/11/2016.
+ */
+
+public class Calculator extends LinearLayout {
+    final String TAG = "Simple Calculator";
+    Button btnClear;
+    Button btnBack;
+    Button btnZero;
+    Button btnOne;
+    Button btnTwo;
+    Button btnThree;
+    Button btnFour;
+    Button btnFive;
+    Button btnSix;
+    Button btnSeven;
+    Button btnEight;
+    Button btnNine;
+    Button btnMultiply;
+    Button btnSubtract;
+    Button btnDecimal;
+    Button btnEquals;
+    Button btnAdd;
+    Button btnDivide;
+    TextView screen;
+    TextView txtHistory;
+    String history;
+    float number1;
+    float number2;
+    float result;
+    Boolean firstTime;
+    String operationScreen;
+    Context context;
+
+    public Calculator(Context context, int style) {
+        super(context);
+
+        this.context = context;
+        Log.d(TAG, "Welcome " + TAG);
+        inflate(context, R.layout.calculator, this);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        btnSeven = (Button) findViewById(R.id.btnSeven);
+        btnEight = (Button) findViewById(R.id.btnEight);
+        btnNine = (Button) findViewById(R.id.btnNine);
+        btnDivide = (Button) findViewById(R.id.btnDivide);
+        btnFour = (Button) findViewById(R.id.btnFour);
+        btnFive = (Button) findViewById(R.id.btnFive);
+        btnSix = (Button) findViewById(R.id.btnSix);
+        btnMultiply = (Button) findViewById(R.id.btnMultiply);
+        btnOne = (Button) findViewById(R.id.btnOne);
+        btnTwo = (Button) findViewById(R.id.btnTwo);
+        btnThree = (Button) findViewById(R.id.btnThree);
+        btnSubtract = (Button) findViewById(R.id.btnSubtract);
+        btnDecimal = (Button) findViewById(R.id.btnDecimal);
+        btnZero = (Button) findViewById(R.id.btnZero);
+        btnEquals = (Button) findViewById(R.id.btnEquals);
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnClear.setOnClickListener(onClickListener);
+        btnBack.setOnClickListener(onClickListener);
+        btnSeven.setOnClickListener(onClickListener);
+        btnEight.setOnClickListener(onClickListener);
+        btnNine.setOnClickListener(onClickListener);
+        btnDivide.setOnClickListener(onClickListener);
+        btnFour.setOnClickListener(onClickListener);
+        btnFive.setOnClickListener(onClickListener);
+        btnSix.setOnClickListener(onClickListener);
+        btnMultiply.setOnClickListener(onClickListener);
+        btnOne.setOnClickListener(onClickListener);
+        btnTwo.setOnClickListener(onClickListener);
+        btnThree.setOnClickListener(onClickListener);
+        btnSubtract.setOnClickListener(onClickListener);
+        btnDecimal.setOnClickListener(onClickListener);
+        btnZero.setOnClickListener(onClickListener);
+        btnEquals.setOnClickListener(onClickListener);
+        btnAdd.setOnClickListener(onClickListener);
+        screen = (TextView) findViewById(R.id.txtScreen);
+        screen.setOnClickListener(onClickListener);
+        txtHistory = (TextView) findViewById(R.id.txtHistory);
+        operationScreen = "";
+        number1 = number2 = result = 0;
+        firstTime = true;
+    }
+
+    public Calculator(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private OnClickListener onClickListener = new OnClickListener() {
+        @Override
+        public void onClick(final View v) {
+            Button b;
+            if (firstTime) {
+                if (v.getId() != R.id.btnOne && v.getId() != R.id.btnTwo && v.getId() != R.id.btnThree && v.getId() != R.id.btnFour && v.getId() != R.id.btnFive
+                        && v.getId() != R.id.btnSix && v.getId() != R.id.btnSeven && v.getId() != R.id.btnEight && v.getId() != R.id.btnNine && v.getId() != R.id.btnDecimal) {
+                    return;
+                } else {
+                    b = (Button) v;
+                    String buttonText = b.getText().toString();
+                    firstTime = false;
+                    float value;
+                    if (v.getId() == R.id.btnDecimal)
+                        value = 0;
+                    else
+                        value = Float.valueOf(b.getText().toString());
+                    screen.setText(b.getText().toString());
+                    number1 = value;
+                    screen.setHint("0");
+
+                }
+            } else {
+                if (v.getId() == R.id.btnClear) {
+                    screen.setText("");
+                    screen.setHint("0");
+                    txtHistory.setText("");
+                    operationScreen = "";
+                    number1 = number2 = result = 0;
+                    firstTime = true;
+                } else if (v.getId() == R.id.btnBack) {
+                    if (!screen.getText().toString().trim().isEmpty())
+                        if (screen.getText().toString().length() == 1) {
+                            screen.setText("");
+                            number1 = 0;
+                        } else {
+                            screen.setText(screen.getText().toString().substring(0, screen.getText().toString().length() - 1));
+                            number1 = Float.valueOf(screen.getText().toString());
+                        }
+
+                } else {
+                    if (v.getId() == R.id.btnOne || v.getId() == R.id.btnTwo || v.getId() == R.id.btnThree || v.getId() == R.id.btnFour || v.getId() == R.id.btnFive
+                            || v.getId() == R.id.btnSix || v.getId() == R.id.btnSeven || v.getId() == R.id.btnEight || v.getId() == R.id.btnNine || v.getId() == R.id.btnZero || v.getId() == R.id.btnDecimal) {
+                        b = (Button) v;
+                        String buttonText = b.getText().toString();
+                        if (v.getId() == R.id.btnDecimal) {
+                            if(screen.getText().toString().contains("."))
+                                return;
+                            if (screen.getText().toString().equals("0.0") || screen.getText().toString().equals("0") || screen.getText().toString().equals(".") || screen.getText().toString().equals("0.")|| screen.getText().toString().equals(""))
+                                screen.setText("0.");
+                        }
+                        String screenT =screen.getText().toString()+ buttonText;
+                        screenT=screenT.replace("..",".");
+                        screen.setText(screenT);
+                        number1 = Float.parseFloat(screenT);
+                    } else {
+                        if (v.getId() == R.id.btnAdd || v.getId() == R.id.btnMultiply || v.getId() == R.id.btnSubtract || v.getId() == R.id.btnDivide) {
+                            b = (Button) v;
+                            if (txtHistory.getText().toString() != "" && screen.getText().toString().equals("")) {
+                                if (txtHistory.getText().toString().charAt(txtHistory.getText().toString().length() - 1) == '+'
+                                        || txtHistory.getText().toString().charAt(txtHistory.getText().toString().length() - 1) == '-'
+                                        || txtHistory.getText().toString().charAt(txtHistory.getText().toString().length() - 1) == 'X'
+                                        || txtHistory.getText().toString().charAt(txtHistory.getText().toString().length() - 1) == '/') {
+                                    txtHistory.setText(txtHistory.getText().toString().substring(0, txtHistory.getText().toString().length() - 1) + b.getText().toString());
+                                    operationScreen = b.getText().toString();
+                                    screen.setText("");
+                                    return;
+                                }
+                            }
+                            txtHistory.setText(txtHistory.getText() + String.valueOf(number1) + b.getText().toString());
+                            if (operationScreen == "") {
+                                screen.setHint("0");
+                                operationScreen = b.getText().toString();
+                                number2 = number1;
+                                number1 = 0;
+                                screen.setText("");
+                            } else {
+                                operation();
+                                operationScreen = b.getText().toString();
+                                screen.setText("");
+                                screen.setHint(String.valueOf(result));
+                                number2 = result;
+                                number1 = result = 0;
+                            }
+                        } else {
+                            if (v.getId() == R.id.btnEquals) {
+                                operation();
+                                screen.setHint(String.valueOf(result));
+                                screen.setText("");
+                                txtHistory.setText("");
+                                operationScreen = "";
+                                number1 = result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    };
+
+    private void operation() {
+        switch (operationScreen) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number2 - number1;
+                break;
+            case "X":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number2 / number1;
+                break;
+        }
+    }
+
+    public int Animation(int form) {
+        switch (form) {
+            case 1:
+                form = R.style.DialogAnimation_RightToLeft;
+                break;
+            case 2:
+                form = R.style.DialogAnimation_DownToUp;
+                break;
+            case 0:
+            default:
+                break;
+        }
+        return form;
+    }
+}
+
